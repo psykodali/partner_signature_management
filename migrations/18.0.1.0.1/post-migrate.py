326e7dfc9d9bcf60c8a442bac14df6a4b5c43275
Dali <psykodali@gmail.com>
2026-02-12 14:37:07 +0100

    fix: post migrate fix

--- migrations/18.0.1.0.1/post-migrate.py
+++ migrations/18.0.1.0.1/post-migrate.py
@@ -6,16 +6,28 @@ def migrate(cr, version):
     # Force recomputation of all new fields
     from odoo import registry, SUPERUSER_ID
     
+    from odoo import api, SUPERUSER_ID
+    
     reg = registry(cr.dbname)
     with reg.cursor() as new_cr:
-        env = reg['ir.model.data'].browse(new_cr, SUPERUSER_ID, []).env
+        env = api.Environment(new_cr, SUPERUSER_ID, {})
         
-        # Recompute all partner fields
-        partners = env['res.partner'].search([])
-        if partners:
-            partners._compute_own_signature_transaction_count()
-            partners._compute_own_signature_transaction_count_this_year()
-            partners._compute_own_paid_transactions_this_year()
-            partners._compute_own_pending_transactions()
-            partners._compute_pending_transactions()
-            partners._compute_forecast_transaction_price()
+        # Recompute all partner fields if they exist
+        Partner = env['res.partner']
+        fields_to_compute = [
+            'own_signature_transaction_count',
+            'own_signature_transaction_count_this_year',
+            'own_paid_transactions_this_year',
+            'own_pending_transactions',
+            'pending_transactions',
+            'forecast_transaction_price'
+        ]
+        
+        # Filter only fields that actually exist in the model
+        valid_fields = [f for f in fields_to_compute if f in Partner._fields]
+        
+        if valid_fields:
+            partners = Partner.search([])
+            if partners:
+                for field in valid_fields:
+                    Partner._recompute_recordset(partners._fields[field])
